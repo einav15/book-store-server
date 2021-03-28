cbf48148b162214dbd2216f634a13303e152c792
Einav Bar <einavbar88@gmail.com>
2021-03-28 18:28:34 +0300

    whitelist remove

--- src/App.js
+++ src/App.js
@@ -4,21 +4,6 @@ const BooksRouter = require('./routers/BooksRouter')
 const UsersRouter = require('./routers/UsersRouter')
 const app = express()
 
-const whiteList = [
-    "31.154.49.171",
-    "http://einav-book-store.s3-website-eu-west-1.amazonaws.com/"
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whiteList.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-};
-
 require('./db/mongoose')
 
 app.use(express.json())
