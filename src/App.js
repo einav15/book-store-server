f19fae30fd363e89c1311cd06943088f4f22b930
Einav Bar <einavbar88@gmail.com>
2021-03-28 17:43:38 +0300

    whitelist add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ const BooksRouter = require('./routers/BooksRouter')
 const UsersRouter = require('./routers/UsersRouter')
 const app = express()
 
+const whiteList = [
+    "http://localhost:3000",
+    "https://git.heroku.com/arcane-hamlet-07739.git",
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whiteList.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
+
 require('./db/mongoose')
 
 app.use(express.json())
